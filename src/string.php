bab00b77abda6ef4fab75ca0c3fa7f66e0ce3b28
Slava Basko <vbasko@xmlshop.com>
2022-11-04 19:31:34 -0400

    string functions added / commonly used (by me)

---
+++ src/string.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Basko\Functional;
+
+/**
+ * Alias of `explode`.
+ *
+ * @param string $separator
+ * @param string $string
+ * @return callable|false|string[]
+ */
+function str_split($separator, $string = null)
+{
+    if (is_null($string)) {
+        return partial(str_split, $separator);
+    }
+
+    return explode($separator, $string);
+}
+
+define('Basko\Functional\str_split', __NAMESPACE__ . '\\str_split');
+
+/**
+ * Alias of native `str_replace`
+ *
+ * Use `partial_p` if you need $count argument:
+ * $f = partial_p('str_replace', [
+ *  1 => $search,
+ *  2 => $replace,
+ *  4 => &$count
+ * ]);
+ *
+ * @param array|string $search
+ * @param array|string $replace
+ * @param array|string $subject
+ * @return array|string|string[]|callable
+ */
+function str_replace($search, $replace = null, $subject = null)
+{
+    if (is_null($replace) && is_null($subject)) {
+        return partial(str_replace, $search);
+    } elseif (is_null($subject)) {
+        return partial(str_replace, $search, $replace);
+    }
+
+    return \str_replace($search, $replace, $subject);
+}
+
+define('Basko\Functional\str_replace', __NAMESPACE__ . '\\str_replace');
+
+/**
+ * Checks if `$string` starts with `$token`.
+ *
+ * @param $token
+ * @param $string
+ * @return bool|callable
+ */
+function str_starts_with($token, $string = null)
+{
+    if (is_null($string)) {
+        return partial(str_starts_with, $token);
+    }
+
+    return strlen($token) <= strlen($string) && substr($string, 0, strlen($token)) === $token;
+}
+
+define('Basko\Functional\str_starts_with', __NAMESPACE__ . '\\str_starts_with');
+
+/**
+ * Checks if `$string` ends with `$token`.
+ *
+ * @param $token
+ * @param $string
+ * @return bool|callable
+ */
+function str_ends_with($token, $string = null)
+{
+    if (is_null($string)) {
+        return partial(str_ends_with, $token);
+    }
+
+    return strlen($token) <= strlen($string) && substr($string, -strlen($token)) === $token;
+}
+
+define('Basko\Functional\str_ends_with', __NAMESPACE__ . '\\str_ends_with');
+
+/**
+ * Checks if a string matches a regular expression.
+ *
+ * @param $pattern
+ * @param $string
+ * @return bool|callable
+ */
+function str_test($pattern, $string = null)
+{
+    if (is_null($string)) {
+        return partial(str_test, $pattern);
+    }
+
+    return 1 === preg_match($pattern, $string);
+}
+
+define('Basko\Functional\str_test', __NAMESPACE__ . '\\str_test');
+
+/**
+ * Alias of `str_pad`.
+ *
+ * @param $length
+ * @param $pad_string
+ * @param $string
+ * @return callable|string
+ */
+function str_pad_left($length, $pad_string = null, $string = null)
+{
+    if (is_null($pad_string) && is_null($string)) {
+        return partial(str_pad_left, $length);
+    } elseif (is_null($string)) {
+        return partial(str_pad_left, $length, $pad_string);
+    }
+
+    return str_pad($string, $length, $pad_string, STR_PAD_LEFT);
+}
+
+define('Basko\Functional\str_pad_left', __NAMESPACE__ . '\\str_pad_left');
+
+/**
+ * Alias of `str_pad`.
+ *
+ * @param $length
+ * @param $pad_string
+ * @param $string
+ * @return callable|string
+ */
+function str_pad_right($length, $pad_string = null, $string = null)
+{
+    if (is_null($pad_string) && is_null($string)) {
+        return partial(str_pad_right, $length);
+    } elseif (is_null($string)) {
+        return partial(str_pad_right, $length, $pad_string);
+    }
+
+    return str_pad($string, $length, $pad_string, STR_PAD_RIGHT);
+}
+
+define('Basko\Functional\str_pad_right', __NAMESPACE__ . '\\str_pad_right');
---
+++ tests/StringTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Functional;
+
+use Basko\Functional as f;
+
+class StringTest extends BaseTest
+{
+    public function test_split()
+    {
+        $words = f\str_split(' ');
+        $this->assertEquals(['Hello', 'World'], $words('Hello World'));
+    }
+
+    public function test_str_replace()
+    {
+        $string = 'a b c d e f';
+        $noSpace = f\str_replace(' ', '');
+        $this->assertEquals('abcdef', $noSpace($string));
+        $this->assertEquals('cdef', f\str_replace(['a', 'b', ' '], '', $string));
+        $this->assertEquals('xbcdyf', f\str_replace(['a', 'e', ' '], ['x', 'y', ''], $string));
+    }
+
+    public function test_str_starts_with() {
+        $http = f\str_starts_with('http://');
+        $this->assertTrue($http('http://gitbub.com'));
+        $this->assertFalse($http('gitbub.com'));
+    }
+
+    public function test_str_ends_with() {
+        $dotCom = f\str_ends_with('.com');
+        $this->assertTrue($dotCom('http://gitbub.com'));
+        $this->assertFalse($dotCom('php.net'));
+    }
+
+    public function test_str_test() {
+        $numeric = f\str_test('/^[0-9.]+$/');
+        $this->assertTrue($numeric('123.43'));
+        $this->assertFalse($numeric('12a3.43'));
+    }
+
+    public function test_str_pad_left()
+    {
+        $padZero = f\str_pad_left('6', '0');
+        $this->assertEquals('000481', $padZero('481'));
+    }
+
+    public function test_str_pad_right()
+    {
+        $padZero = f\str_pad_right('6', '0');
+        $this->assertEquals('481000', $padZero('481'));
+    }
+}
